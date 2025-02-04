418683169041debbcfa7313efdb45cb9edbb0e1d
LVDark <alibeyozbay@hotmail.com>
2025-02-04 19:14:10 +0300

    Day 1 is completed

---
+++ Day1/day_1.py
@@ -0,0 +1,5 @@
+#Day 1. Creating a simple band name generator
+print("Welcome to the Band Name Generator!")
+city = input("Which city did you grow up in?")
+pet = input("What is the name of your first pet?")
+print("Your Band Name is: " + city + " " + pet)
